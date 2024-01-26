a1e0b5591c69061762e0038815555e48c8f5642b
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-26 04:02:53 -0500

    Update SnippetReferralAccount.tsx

--- src/components/V2FeatureButton/snippet/SnippetReferralAccount.tsx
+++ src/components/V2FeatureButton/snippet/SnippetReferralAccount.tsx
@@ -9,10 +9,7 @@ import prettierPluginTypescript from 'prettier/plugins/typescript';
 
 const SnippetReferralAccount = () => {
   const [snippet, setSnippet] = useState(``);
-  const unformattedSnippet = `
-  <iframe id='iframe-widget' src='https://changenow.io/embeds/exchange-widget/v2/widget.html?FAQ=false&amount=1&amountFiat&backgroundColor=2B2B35&darkMode=true&from=sol&horizontal=false&isFiat=false&lang=en-US&link_id=b0d09e3f1142fa&locales=true&logo=false&primaryColor=9945FF&to=usdc&toTheMoon=false' style="height: 356px; width: 100%; border: none"></iframe>
-    <script defer type='text/javascript' src='https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js'></script>
-`;
+  const unformattedSnippet = ``;
 
   useEffect(() => {
     prettier
@@ -27,9 +24,9 @@ const SnippetReferralAccount = () => {
 
   return (
     <div className='flex w-full justify-center text-left'>
-      <SyntaxHighlighter language="typescript" showLineNumbers style={vs2015}>
-        {snippet}
-      </SyntaxHighlighter>
+      
+  <iframe id='iframe-widget' src='https://changenow.io/embeds/exchange-widget/v2/widget.html?FAQ=false&amount=1&amountFiat&backgroundColor=2B2B35&darkMode=true&from=sol&horizontal=false&isFiat=false&lang=en-US&link_id=b0d09e3f1142fa&locales=true&logo=false&primaryColor=9945FF&to=usdc&toTheMoon=false' style="height: 356px; width: 100%; border: none"></iframe>
+    <script defer type='text/javascript' src='https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js'></script>
     </div>
   );
 };
