34acd23d81205a6166842f35b3dfac0eacf2d8bf
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-25 22:13:07 -0500

    Update JupiterLogo.tsx

--- src/icons/JupiterLogo.tsx
+++ src/icons/JupiterLogo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const JupiterLogo: React.FC<{ width?: number; height?: number }> = ({ width = 24, height = 24 }) => {
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src={'https://getmoneytoken.xyz/assets/images/logo/logo.png'} width={width} height={height} alt="Get Money aggregator" />;
+  return <img src={'https://cryptoexchange.vip/assets/img/content/cta_thumb.png'} width={width} height={height} alt="Get Money aggregator" />;
 };
 
 export default JupiterLogo;
