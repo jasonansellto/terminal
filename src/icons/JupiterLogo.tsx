c3059c8cac96236631065110f1424899beacb231
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-26 01:15:43 -0500

    Update JupiterLogo.tsx

--- src/icons/JupiterLogo.tsx
+++ src/icons/JupiterLogo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const JupiterLogo: React.FC<{ width?: number; height?: number }> = ({ width = 24, height = 24 }) => {
   // eslint-disable-next-line @next/next/no-img-element
-  return <img src={'https://solana.com/_next/static/media/logotype.e4df684f.svg'} width={width} height={height} alt="Solana aggregator" />;
+  return <img src={'https://solana.com/src/img/branding/solanaLogoMark.svg'} width={width} height={height} alt="Solana aggregator" />;
 };
 
 export default JupiterLogo;
