ba858e68efb2fa67ea3cd3c8d824a2e9ac636852
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-25 22:17:56 -0500

    Update AppHeader.tsx

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -34,7 +34,7 @@ const AppHeader: React.FC<{}> = () => {
           <Link href="https://jup.ag" shallow className="flex-1">
             <h1 className="flex items-center text-lg font-semibold text-white">
               <JupiterLogo />
-              <span className="ml-3">Jupiter</span>
+              <span className="ml-3">VIP Exchange</span>
             </h1>
           </Link>
         </div>
