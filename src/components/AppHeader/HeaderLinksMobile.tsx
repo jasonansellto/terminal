505230fb01fc28751c18da329caedbc08c22b033
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-26 01:26:40 -0500

    Update HeaderLinksMobile.tsx

--- src/components/AppHeader/HeaderLinksMobile.tsx
+++ src/components/AppHeader/HeaderLinksMobile.tsx
@@ -38,13 +38,13 @@ const HeaderLinksMobile: React.FC = () => {
         href="#"
         external
         label={'Link 1'}
-        icon={<RepoLogo width="20" height="20" />}
+        icon={<JupiterLogo width="20" height="20" />}
       />
       <HeaderLink
         href="#"
         external
         label={'Link 2'}
-        icon={<DiscordIcon width="20" height="20" />}
+        icon={<TwitterIcon width="20" height="20" />}
       />
     </div>
   );
