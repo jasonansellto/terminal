b7770bc3713604c43d6fc9cdf8f6a7fcc4af50b8
jasonansellto <94579466+jasonansellto@users.noreply.github.com>
2024-01-25 23:54:38 -0500

    Update ModalTerminal.tsx

--- src/content/ModalTerminal.tsx
+++ src/content/ModalTerminal.tsx
@@ -34,13 +34,7 @@ const ModalTerminal = (props: {
   }, [passthroughWalletContextState, props]);
 
   return (
-    <div
-      className="p-4 hover:bg-white/10 rounded-xl cursor-pointer flex h-full w-full flex-col items-center justify-center text-white"
-      onClick={launchTerminal}
-    >
-      <WalletDisconnectedGraphic />
-      <span className="text-xs mt-4">Launch Terminal Modal</span>
-    </div>
+    <div id="integrated-terminal"></div>
   );
 };
 
